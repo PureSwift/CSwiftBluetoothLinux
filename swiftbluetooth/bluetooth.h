ebd2ea5df4c354a20b3581d0e113db07eb2ddbc0
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-03-14 02:41:32 -0500

    added swift_bluetooth_hci_set_bit()

--- swiftbluetooth/bluetooth.h
+++ swiftbluetooth/bluetooth.h
@@ -12,3 +12,9 @@ static inline int swift_bluetooth_ioctl(int socket, int command, void * pointer)
 {
     return ioctl(socket, command, pointer);
 }
+
+static inline void swift_bluetooth_hci_set_bit(int nr, void *addr)
+{
+	*((uint32_t *) addr + (nr >> 5)) |= (1 << (nr & 31));
+}
+
