43baf6bded44978b76ba53833190f17adbb20281
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-04-05 18:46:09 -0500

    updated C helper header

--- swiftbluetooth/bluetooth.h
+++ swiftbluetooth/bluetooth.h
@@ -7,14 +7,14 @@
  */
 
 #include <stdint.h>
+#include <sys/ioctl.h>
 
-static inline int swift_bluetooth_ioctl(int socket, int command, void * pointer)
+static inline int swift_bluetooth_ioctl (int fd, unsigned long int request, void *pointer)
 {
-    return ioctl(socket, command, pointer);
+    return ioctl(fd, request, pointer);
 }
 
 static inline void swift_bluetooth_hci_set_bit(int nr, void *addr)
 {
-	*((uint32_t *) addr + (nr >> 5)) |= (1 << (nr & 31));
+    *((uint32_t *) addr + (nr >> 5)) |= (1 << (nr & 31));
 }
-
